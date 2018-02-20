4fc138a38ab9243c4bd249ee493219c07f297530
BHARTI ARORA <bhartiarora2810@gmail.com>
2018-02-21 00:41:49 +0530

    redirecting from login page to other issue solved

--- loginserv.php
+++ loginserv.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-$error=''; //Variable to Store error message;
+$error='';
 if(isset($_POST['submit'])){
  if(empty($_POST['user']) || empty($_POST['pass'])){
  $error = "Username or Password is Invalid";
@@ -15,10 +15,11 @@ include 'dbh.php';
  $query = mysqli_query($conn, "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'");
  
   $rows = mysqli_num_rows($query);
+  echo "before block";
  if($rows == 1){
      echo "yes";
      header('Location: welcome.php');
-     exit();// Redirecting to other page
+     //exit();// Redirecting to other page
     }
  
  else
